6c5aa76aac813e0ffcdefbb027d30352c71d1672
paul-frdev <pavelzadyrko@gmail.com>
2022-05-11 17:12:12 +0300

    fix: login-form

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -3,9 +3,38 @@ import styles from './loginPage.module.css';
 import { Input } from 'common/fields';
 import { Button } from 'common/buttons';
 
+
+const validateIsEmpty = (value: string) => {
+  if (!value) return 'field required';
+  return null;
+}
+const validateUserName = (value: string) => {
+  return validateIsEmpty(value);
+};
+
+const validatePassword = (value: string) => {
+  return validateIsEmpty(value);
+};
+
+const loginFormValidateSchema = {
+  username: validateUserName,
+  password: validatePassword
+};
+
+const validateLoginForm = (name: keyof typeof loginFormValidateSchema, value: string) => {
+  return loginFormValidateSchema[name](value);
+}
+
+
+interface FormErrors {
+  username: string | null;
+  password: string | null;
+}
+
 export const LoginPage = () => {
 
   const [formValues, setFormValues] = useState({ username: '', password: '' });
+  const [formErrors, setFormErrors] = useState<FormErrors>({ username: null, password: null });
 
   return (
     <div className={styles.loginPage}>
@@ -17,13 +46,21 @@ export const LoginPage = () => {
           <div className={styles.inputForm}>
             <Input
               className={styles.input}
-              isError={false}
-              helperText={`Validation`}
               value={formValues.username}
               type='text'
               placeholder='Username'
               name='username'
-              onChange={(event) => setFormValues({ ...formValues, username: event.currentTarget.value })}
+              onChange={(event) => {
+                const username = event.currentTarget.value;
+                setFormValues({ ...formValues, username });
+
+                const error = validateLoginForm('username', username);
+                setFormErrors({ ...formErrors, username: error });
+              }}
+              {...(!!formErrors.username && {
+                isError: !!formErrors.username,
+                helperText: formErrors.username
+              })}
             />
           </div>
           <div className={styles.inputForm}>
@@ -33,7 +70,17 @@ export const LoginPage = () => {
               placeholder='password'
               type='password'
               name='password'
-              onChange={(event) => setFormValues({ ...formValues, password: event.currentTarget.value })}
+              onChange={(event) => {
+                const password = event.currentTarget.value;
+                setFormValues({ ...formValues, password });
+
+                const error = validateLoginForm('password', password);
+                setFormErrors({ ...formErrors, password: error });
+              }}
+              {...(!!formErrors.password && {
+                isError: !!formErrors.password,
+                helperText: formErrors.password
+              })}
             />
           </div>
           <div>
@@ -41,7 +88,9 @@ export const LoginPage = () => {
           </div>
           <Button className={styles.loginButton}>Sign In</Button>
         </form>
-        <div></div>
+        <div className={styles.SignupContainer}>
+          <a href="#">Create new account</a>
+        </div>
       </div>
     </div>
   );
